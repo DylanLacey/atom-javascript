e4a7ffaf8e898ce0ddced56a40b9e86472e5ad9d
Evhen Popenko <evhen.popenko@ironsrc.com>
2016-05-18 16:58:42 +0300

    add sauceLabs test into karma; fix travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,85 @@
-// Karma configuration
-// Generated on Tue May 17 2016 14:33:19 GMT+0300 (EEST)
-var fs = require('fs');
-
 module.exports = function(config) {
+
+  var customLaunchers = {
+    'SL_CHROME': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '50'
+    },
+    'SL_FIREFOX': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '45'
+    },
+    'SL_SAFARI7': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.9',
+      version: '7'
+    },
+    'SL_SAFARI8': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.10',
+      version: '8'
+    },
+    'SL_SAFARI9': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.11',
+      version: '9.0'
+    },
+    'SL_IOS8': {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.10',
+      version: '8.4'
+    },
+    'SL_IOS9': {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.10',
+      version: '9.1'
+    },
+    'SL_IE9': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2008',
+      version: '9'
+    },
+    'SL_IE10': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2012',
+      version: '10'
+    },
+    'SL_IE11': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    },
+    'SL_EDGE': {
+      base: 'SauceLabs',
+      browserName: 'MicrosoftEdge',
+      platform: 'Windows 10',
+      version: '13.10586'
+    },
+    'SL_ANDROID4.1': {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '4.1'
+    },
+    'SL_ANDROID5': {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '5.1'
+    }
+  };
+
+
   config.set({
     basePath: '',
     frameworks: ['mocha',  'browserify', 'sinon-chai'],
@@ -24,7 +101,7 @@ module.exports = function(config) {
     browserify: {
       debug: true,
       transform: [
-       [
+        [
           'browserify-istanbul',
           {
             instrumenterConfig: {
@@ -40,16 +117,34 @@ module.exports = function(config) {
         {'type': 'lcov'}
       ]
     },
-    reporters: ['progress', 'mocha', 'coverage'],
+    reporters: ['progress', 'mocha', 'coverage', 'saucelabs'],
     port: 9876,
     colors: true,
     logLevel: 'info',
     autoWatch: false,
     sauceLabs: {
       testName: 'IronSource Atom js',
-      startConnect: false
+      retryLimit: 1,
+      recordVideo: false,
+      recordScreenshots: false,
+      options: {
+        'selenium-version': '2.53.0',
+        'command-timeout': 600,
+        'idle-timeout': 600,
+        'max-duration': 5400
+      },
+      build: process.env.TRAVIS_BUILD_NUMBER,
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+      username: process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
+      startConnect: true
     },
-    browsers: ['PhantomJS'],
+    captureTimeout: 300000,
+    browserDisconnectTimeout : 90000,
+    browserDisconnectTolerance : 3,
+    browserNoActivityTimeout : 90000,
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
     singleRun: true
   })
 };
