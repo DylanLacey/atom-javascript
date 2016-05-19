a88742381604a9baa02ed7dbe5f5f98dac93e70f
Evhen Popenko <evhen.popenko@ironsrc.com>
2016-05-19 16:20:02 +0300

    clear comments

--- karma.conf.js
+++ karma.conf.js
@@ -17,66 +17,24 @@ module.exports = function(config) {
       platform: 'OS X 10.9',
       version: '7'
     },
-    // 'SL_SAFARI8': {
-    //   base: 'SauceLabs',
-    //   browserName: 'safari',
-    //   platform: 'OS X 10.10',
-    //   version: '8'
-    // },
-    // 'SL_SAFARI9': {
-    //   base: 'SauceLabs',
-    //   browserName: 'safari',
-    //   platform: 'OS X 10.11',
-    //   version: '9.0'
-    // },
     'SL_IOS8': {
       base: 'SauceLabs',
       browserName: 'iphone',
       platform: 'OS X 10.10',
       version: '8.4'
     },
-    // 'SL_IOS9': {
-    //   base: 'SauceLabs',
-    //   browserName: 'iphone',
-    //   platform: 'OS X 10.10',
-    //   version: '9.1'
-    // },
-    // 'SL_IE9': {
-    //   base: 'SauceLabs',
-    //   browserName: 'internet explorer',
-    //   platform: 'Windows 2008',
-    //   version: '9'
-    // },
     'SL_IE10': {
       base: 'SauceLabs',
       browserName: 'internet explorer',
       platform: 'Windows 2012',
       version: '10'
     },
-    // 'SL_IE11': {
-    //   base: 'SauceLabs',
-    //   browserName: 'internet explorer',
-    //   platform: 'Windows 8.1',
-    //   version: '11'
-    // },
-    // 'SL_EDGE': {
-    //   base: 'SauceLabs',
-    //   browserName: 'MicrosoftEdge',
-    //   platform: 'Windows 10',
-    //   version: '13.10586'
-    // },
     'SL_ANDROID4.1': {
       base: 'SauceLabs',
       browserName: 'android',
       platform: 'Linux',
       version: '4.1'
-    },
-    // 'SL_ANDROID5': {
-    //   base: 'SauceLabs',
-    //   browserName: 'android',
-    //   platform: 'Linux',
-    //   version: '5.1'
-    // }
+    }
   };
 
 
@@ -127,12 +85,6 @@ module.exports = function(config) {
       retryLimit: 1,
       recordVideo: false,
       recordScreenshots: false,
-      // options: {
-      //   'selenium-version': '2.53.0',
-      //   'command-timeout': 600,
-      //   'idle-timeout': 600,
-      //   'max-duration': 5400
-      // },
       build: process.env.TRAVIS_BUILD_NUMBER,
       tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
       username: process.env.SAUCE_USERNAME,
